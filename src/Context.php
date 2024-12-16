485b0f9b8a64ed21e4aba94198f6e5ae84a4836c
Jason Silkey <jason@jasonsilkey.com>
2024-12-15 20:45:04 -0800

    fix missing semicolon

--- src/Context.php
+++ src/Context.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Idiosyncratic\GraphQL\Server;
 
-use ArrayIterator
-
+use ArrayIterator;
 use Countable;
 use IteratorAggregate;
 use Psr\Http\Message\ServerRequestInterface;
